49cf8451368cb059f96c645948f37dd449880a2a
konatebeh20 <konatebeh20@gmail.com>
2025-05-10 23:17:23 +0000

    Initial commit

---
+++ wayna/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wayna',
+  webDir: 'www'
+};
+
+export default config;
